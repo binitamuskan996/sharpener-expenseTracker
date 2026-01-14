0f6bcb5d90451e46c8d6e9e9b44f9e411ba75fcc
binitamuskan996 <binitabini05@gmail.com>
2026-01-14 15:55:55 +0530

    add dynamic pagination

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -59,7 +59,7 @@ const getExpenses = async (req, res) => {
   try {
     const page = parseInt(req.query.page) || 1;
     const limit = parseInt(req.query.limit) || 10;
-
+    
     const offset = (page - 1) * limit;
 
     const { count, rows } = await Expense.findAndCountAll({
