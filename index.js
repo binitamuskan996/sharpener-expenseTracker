32d57203ca587da8e9107377e97c7acde70b9d33
binitamuskan996 <binitabini05@gmail.com>
2026-01-15 16:19:33 +0530

    Add all project files

--- index.js
+++ index.js
@@ -1,30 +1,43 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const path = require('path');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const { GoogleGenAI } = require('@google/genai'); 
-
+// const helmet=require('helmet');
+// const compression=require('compression');
+const morgan = require('morgan');
+const { GoogleGenAI } = require('@google/genai');
 dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
 const sequelize = require('./utils/db-connection');
 
 const authRoutes = require('./routes/authRoutes');
 const expenseRoutes = require('./routes/expenseRoutes');
 const premiumRoutes = require('./routes/premiumRoute');
-const forgotpasswordRoutes=require('./routes/forgotpasswordRoutes');
+const forgotpasswordRoutes = require('./routes/forgotpasswordRoutes');
 
 const User = require('./models/user');
 const Expense = require('./models/expense');
 const Order = require('./models/order');
 const ForgotPasswordRequest = require('./models/forgotpassword');
+const Stream = require('stream');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+//app.use(helmet());
+//app.use(compression());
+app.use(morgan('combined', { stream: accessLogStream }))
+
 app.use('/dashboard', authRoutes);
 app.use('/expense', expenseRoutes);
 app.use('/premium', premiumRoutes);
-app.use('/password',forgotpasswordRoutes);
+app.use('/password', forgotpasswordRoutes);
 
 User.hasMany(Expense);
 Expense.belongsTo(User);
@@ -35,10 +48,10 @@ Order.belongsTo(User);
 User.hasMany(ForgotPasswordRequest);
 ForgotPasswordRequest.belongsTo(User);
 
-sequelize.sync({ force: false}) 
+sequelize.sync({ force: false })
   .then(() => {
-    app.listen(3000, () => {
-      console.log('Server running on http://localhost:3000');
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => {
--- utils/db-connection.js
+++ utils/db-connection.js
@@ -1,7 +1,9 @@
 const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const sequelize = new Sequelize('testdb','root','Binita@123',{
-      host: 'localhost',
+const sequelize = new Sequelize(  process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,{
+      host: process.env.DB_HOST,
       dialect:'mysql'
 });
 
